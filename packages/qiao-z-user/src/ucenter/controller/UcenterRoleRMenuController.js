9bfa0a52e8cb4a056ffa719a51aabfa956f1b4f3
uikoo9 <uikoo9@qq.com>
2022-11-11 12:07:09 +0800

    feat(qiao-z-user): controller ok

--- packages/qiao-z-user/src/ucenter/controller/UcenterRoleRMenuController.js
+++ packages/qiao-z-user/src/ucenter/controller/UcenterRoleRMenuController.js
@@ -1,27 +1,27 @@
 // service
-var service	= require('../service/UcenterRoleRMenuService.js');
+const service = require('../service/UcenterRoleRMenuService.js');
 
 /**
  * ucenter role-r-menu controller
  */
-module.exports = function(app){
-	// ucenter role-r-menu list
-	app.post('/ucenter/role-r-menu/list', function(req, res){
-		service.ucenterRoleRMenuList(req, res);
-	});
+module.exports = (app) => {
+    // ucenter role-r-menu list
+    app.post('/ucenter/role-r-menu/list', (req, res) => {
+        service.ucenterRoleRMenuList(req, res);
+    });
 
-	// ucenter role-r-menu get
-	app.post('/ucenter/role-r-menu/get', function(req, res){
-		service.ucenterRoleRMenuGet(req, res);
-	});
-	
-	// ucenter role-r-menu save
-	app.post('/ucenter/role-r-menu/save', function(req, res){
-		service.ucenterRoleRMenuSave(req, res);
-	});
+    // ucenter role-r-menu get
+    app.post('/ucenter/role-r-menu/get', (req, res) => {
+        service.ucenterRoleRMenuGet(req, res);
+    });
 
-	// ucenter role-r-menu del
-	app.post('/ucenter/role-r-menu/del', function(req, res){
-		service.ucenterRoleRMenuDel(req, res);
-	});
+    // ucenter role-r-menu save
+    app.post('/ucenter/role-r-menu/save', (req, res) => {
+        service.ucenterRoleRMenuSave(req, res);
+    });
+
+    // ucenter role-r-menu del
+    app.post('/ucenter/role-r-menu/del', (req, res) => {
+        service.ucenterRoleRMenuDel(req, res);
+    });
 };
--- packages/qiao-z-user/src/ucenter/controller/UcenterRoleRUserController.js
+++ packages/qiao-z-user/src/ucenter/controller/UcenterRoleRUserController.js
@@ -1,27 +1,27 @@
 // service
-var service	= require('../service/UcenterRoleRUserService.js');
+const service = require('../service/UcenterRoleRUserService.js');
 
 /**
  * ucenter role-r-user controller
  */
-module.exports = function(app){
-	// ucenter role-r-user list
-	app.post('/ucenter/role-r-user/list', function(req, res){
-		service.ucenterRoleRUserList(req, res);
-	});
+module.exports = (app) => {
+    // ucenter role-r-user list
+    app.post('/ucenter/role-r-user/list', (req, res) => {
+        service.ucenterRoleRUserList(req, res);
+    });
 
-	// ucenter role-r-user get
-	app.post('/ucenter/role-r-user/get', function(req, res){
-		service.ucenterRoleRUserGet(req, res);
-	});
-	
-	// ucenter role-r-user save
-	app.post('/ucenter/role-r-user/save', function(req, res){
-		service.ucenterRoleRUserSave(req, res);
-	});
+    // ucenter role-r-user get
+    app.post('/ucenter/role-r-user/get', (req, res) => {
+        service.ucenterRoleRUserGet(req, res);
+    });
 
-	// ucenter role-r-user del
-	app.post('/ucenter/role-r-user/del', function(req, res){
-		service.ucenterRoleRUserDel(req, res);
-	});
+    // ucenter role-r-user save
+    app.post('/ucenter/role-r-user/save', (req, res) => {
+        service.ucenterRoleRUserSave(req, res);
+    });
+
+    // ucenter role-r-user del
+    app.post('/ucenter/role-r-user/del', (req, res) => {
+        service.ucenterRoleRUserDel(req, res);
+    });
 };
