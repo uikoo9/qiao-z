c749e785678b0740a5c020280247d11d58c643be
uikoo9 <uikoo9@qq.com>
2023-04-14 16:32:39 +0800

    feat(u): ava qiao get ip

---
+++ packages/qiao-get-ip/__tests__/test.js
@@ -0,0 +1,26 @@
+// ava
+const test = require('ava');
+
+// jsdom
+const { JSDOM } = require('jsdom');
+
+// ip
+const { getIP } = require('../index.js');
+
+// nodejs
+test.serial('get ip on nodejs', async (t) => {
+  const ip = await getIP();
+  t.log(ip);
+  t.truthy(ip);
+});
+
+
+// browser
+test.serial('get ip on browser', async (t) => {
+  const dom = new JSDOM('', { url: 'http://localhost' });
+  global.document = dom.window.document;
+
+  const ip = await getIP();
+  t.log(ip);
+  t.truthy(ip);
+});
