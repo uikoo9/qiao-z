d0050d443ec108c7cc9ab1495b5d92882fe8ca03
uikoo9 <uikoo9@qq.com>
2023-05-17 11:13:37 +0800

    feat: ncu

--- packages/qiao-get-ip/__tests__/test.js
+++ packages/qiao-get-ip/__tests__/test.js
@@ -7,20 +7,22 @@ const { JSDOM } = require('jsdom');
 // ip
 const { getIP } = require('../index.js');
 
+// options
+const options = {timeout:2000};
+
 // nodejs
 test.serial('get ip on nodejs', async (t) => {
-  const ip = await getIP();
+  const ip = await getIP(options);
   t.log(ip);
   t.truthy(ip);
 });
-
 
 // browser
 test.serial('get ip on browser', async (t) => {
   const dom = new JSDOM('', { url: 'http://localhost' });
   global.document = dom.window.document;
 
-  const ip = await getIP();
+  const ip = await getIP(options);
   t.log(ip);
   t.truthy(ip);
 });
--- packages/qiao-short-link/__tests__/test.js
+++ packages/qiao-short-link/__tests__/test.js
@@ -7,20 +7,22 @@ const { JSDOM } = require('jsdom');
 // link
 const { shortLink } = require('../index.js');
 
+// options
+const options = { timeout:2000 };
+
 // nodejs
 test.serial('get ip on nodejs', async (t) => {
-  const link = await shortLink('https://insistime.com/');
+  const link = await shortLink('https://insistime.com/', options);
   t.log(link);
   t.truthy(link);
 });
-
 
 // browser
 test.serial('get ip on browser', async (t) => {
   const dom = new JSDOM('', { url: 'http://localhost' });
   global.document = dom.window.document;
 
-  const link = await shortLink('https://insistime.com/');
+  const link = await shortLink('https://insistime.com/', options);
   t.log(link);
   t.truthy(link);
 });
