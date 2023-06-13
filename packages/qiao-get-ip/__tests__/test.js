b6c3e3fc7bd9d1b4eedf80864df808d0bc0cbeb8
uikoo9 <uikoo9@qq.com>
2023-06-13 19:19:34 +0800

    feat(qiao-get-ip): test timeout 900ms

--- packages/qiao-get-ip/__tests__/test.js
+++ packages/qiao-get-ip/__tests__/test.js
@@ -9,21 +9,21 @@ const { getIP } = require('../index.js');
 
 // nodejs
 test.serial('get ip on nodejs', async (t) => {
-  t.timeout(450);
+  t.timeout(900);
 
-  const ip = await getIP(500);
+  const ip = await getIP(900);
   t.log(ip);
   t.truthy(ip);
 });
 
 // browser
 test.serial('get ip on browser', async (t) => {
-  t.timeout(450);
+  t.timeout(900);
 
   const dom = new JSDOM('', { url: 'http://localhost' });
   global.document = dom.window.document;
 
-  const ip = await getIP(500);
+  const ip = await getIP(900);
   t.log(ip);
   t.truthy(ip);
 });
