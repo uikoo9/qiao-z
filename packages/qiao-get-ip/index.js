d88a0be5ad622a59860a97bd982057092d3e3f50
uikoo9 <uikoo9@qq.com>
2023-08-04 15:06:36 +0800

    feat: ncu

--- packages/qiao-get-ip/index.js
+++ packages/qiao-get-ip/index.js
@@ -4,9 +4,8 @@ var qiaoAjax = require('qiao-ajax');
 
 const word = '[a-fA-F\\d:]';
 
-const boundry = options => options && options.includeBoundaries
-	? `(?:(?<=\\s|^)(?=${word})|(?<=${word})(?=\\s|$))`
-	: '';
+const boundry = (options) =>
+  options && options.includeBoundaries ? `(?:(?<=\\s|^)(?=${word})|(?<=${word})(?=\\s|$))` : '';
 
 const v4 = '(?:25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]\\d|\\d)(?:\\.(?:25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]\\d|\\d)){3}';
 
@@ -23,19 +22,28 @@ const v6 = `
 (?:${v6segment}:){1}(?:(?::${v6segment}){0,4}:${v4}|(?::${v6segment}){1,6}|:)| // 1::              1::3:4:5:6:7:8   1::8            1::3:4:5:6:7:1.2.3.4
 (?::(?:(?::${v6segment}){0,5}:${v4}|(?::${v6segment}){1,7}|:))             // ::2:3:4:5:6:7:8  ::2:3:4:5:6:7:8  ::8             ::1.2.3.4
 )(?:%[0-9a-zA-Z]{1,})?                                             // %eth0            %1
-`.replace(/\s*\/\/.*$/gm, '').replace(/\n/g, '').trim();
+`
+  .replace(/\s*\/\/.*$/gm, '')
+  .replace(/\n/g, '')
+  .trim();
 
 // Pre-compile only the exact regexes because adding a global flag make regexes stateful
 const v46Exact = new RegExp(`(?:^${v4}$)|(?:^${v6}$)`);
 const v4exact = new RegExp(`^${v4}$`);
 const v6exact = new RegExp(`^${v6}$`);
 
-const ipRegex = options => options && options.exact
-	? v46Exact
-	: new RegExp(`(?:${boundry(options)}${v4}${boundry(options)})|(?:${boundry(options)}${v6}${boundry(options)})`, 'g');
+const ipRegex = (options) =>
+  options && options.exact
+    ? v46Exact
+    : new RegExp(
+      `(?:${boundry(options)}${v4}${boundry(options)})|(?:${boundry(options)}${v6}${boundry(options)})`,
+      'g',
+    );
 
-ipRegex.v4 = options => options && options.exact ? v4exact : new RegExp(`${boundry(options)}${v4}${boundry(options)}`, 'g');
-ipRegex.v6 = options => options && options.exact ? v6exact : new RegExp(`${boundry(options)}${v6}${boundry(options)}`, 'g');
+ipRegex.v4 = (options) =>
+  options && options.exact ? v4exact : new RegExp(`${boundry(options)}${v4}${boundry(options)}`, 'g');
+ipRegex.v6 = (options) =>
+  options && options.exact ? v6exact : new RegExp(`${boundry(options)}${v6}${boundry(options)}`, 'g');
 
 // ajax
 
@@ -47,9 +55,10 @@ ipRegex.v6 = options => options && options.exact ? v6exact : new RegExp(`${bound
  */
 const getIPByWebsite = (url, timeout) => {
   return new Promise((resolve, reject) => {
-    qiaoAjax.get(url, {
-      timeout: timeout,
-    })
+    qiaoAjax
+      .get(url, {
+        timeout: timeout,
+      })
       .then((res) => {
         // check
         if (!res || res.status !== 200 || !res.data) return;
