38468dbf0b355292b29c24453751555bb3140c51
uikoo9 <uikoo9@qq.com>
2024-01-21 19:05:20 +0800

    feat: test app.js

--- packages/qiao-z/__tests__/app.js
+++ packages/qiao-z/__tests__/app.js
@@ -1,10 +1,12 @@
 // qz
 const qz = require('../index.js');
 
-// app
-const app = qz({
-  cron: require('qiao-timer'),
-});
-
-// listen
-app.listen();
+// start
+(async function(){
+  const app = await qz({
+    // cron: require('qiao-timer'),
+  });
+  
+  // listen
+  app.listen();
+})();
