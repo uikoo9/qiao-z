07f75c346ad0443451a9502bfe3eef5d31b90ab8
uikoo9 <uikoo9@qq.com>
2024-07-03 20:52:12 +0800

    feat(qiao-z): proxy ok

--- packages/qiao-z/index.js
+++ packages/qiao-z/index.js
@@ -681,29 +681,62 @@ const logger = qiao_log_js.Logger('qiao-z');
  * @param {*} proxyOptions
  */
 const proxy = (request, response, proxyOptions) => {
+  const methodName = 'res.proxy';
+
+  // check
+  if (!proxyOptions) {
+    logger.error(methodName, 'need proxyOptions');
+    responseError(response);
+    return;
+  }
+  if (!proxyOptions.host) {
+    logger.error(methodName, 'need proxyOptions.host');
+    responseError(response);
+    return;
+  }
+  if (!proxyOptions.port) {
+    logger.error(methodName, 'need proxyOptions.port');
+    responseError(response);
+    return;
+  }
+  if (!proxyOptions.path) {
+    logger.error(methodName, 'need proxyOptions.path');
+    responseError(response);
+    return;
+  }
+
   // options
   const options = {
     hostname: proxyOptions.host,
     port: proxyOptions.port,
-    path: request.url,
+    path: proxyOptions.path,
     method: request.method,
     headers: request.headers,
   };
+  logger.info(methodName, 'options', options);
 
   // proxy
   const proxy = http.request(options, (proxyRes) => {
+    logger.info(methodName, 'proxyRes.statusCode', proxyRes.statusCode);
+    logger.info(methodName, 'proxyRes.headers', proxyRes.headers);
+
     response.writeHead(proxyRes.statusCode, proxyRes.headers);
     proxyRes.pipe(response, { end: true });
   });
   proxy.on('error', (err) => {
-    logger.error('Proxy request error:', err);
-    response.writeHead(500, { 'Content-Type': 'text/plain' });
-    response.end('Something went wrong.');
+    logger.error(methodName, 'proxy request error', err);
+    responseError(response);
   });
 
   // request
   request.pipe(proxy, { end: true });
 };
+
+// response error
+function responseError(response) {
+  response.writeHead(500, { 'Content-Type': 'text/plain' });
+  response.end('Something went wrong.');
+}
 
 // res methods
 
--- packages/qiao-z/src/server/res/res-proxy.js
+++ packages/qiao-z/src/server/res/res-proxy.js
@@ -12,28 +12,61 @@ const logger = Logger('qiao-z');
  * @param {*} proxyOptions
  */
 const proxy = (request, response, proxyOptions) => {
+  const methodName = 'res.proxy';
+
+  // check
+  if (!proxyOptions) {
+    logger.error(methodName, 'need proxyOptions');
+    responseError(response);
+    return;
+  }
+  if (!proxyOptions.host) {
+    logger.error(methodName, 'need proxyOptions.host');
+    responseError(response);
+    return;
+  }
+  if (!proxyOptions.port) {
+    logger.error(methodName, 'need proxyOptions.port');
+    responseError(response);
+    return;
+  }
+  if (!proxyOptions.path) {
+    logger.error(methodName, 'need proxyOptions.path');
+    responseError(response);
+    return;
+  }
+
   // options
   const options = {
     hostname: proxyOptions.host,
     port: proxyOptions.port,
-    path: request.url,
+    path: proxyOptions.path,
     method: request.method,
     headers: request.headers,
   };
+  logger.info(methodName, 'options', options);
 
   // proxy
   const proxy = http.request(options, (proxyRes) => {
+    logger.info(methodName, 'proxyRes.statusCode', proxyRes.statusCode);
+    logger.info(methodName, 'proxyRes.headers', proxyRes.headers);
+
     response.writeHead(proxyRes.statusCode, proxyRes.headers);
     proxyRes.pipe(response, { end: true });
   });
   proxy.on('error', (err) => {
-    logger.error('Proxy request error:', err);
-    response.writeHead(500, { 'Content-Type': 'text/plain' });
-    response.end('Something went wrong.');
+    logger.error(methodName, 'proxy request error', err);
+    responseError(response);
   });
 
   // request
   request.pipe(proxy, { end: true });
 };
+
+// response error
+function responseError(response) {
+  response.writeHead(500, { 'Content-Type': 'text/plain' });
+  response.end('Something went wrong.');
+}
 
 export default proxy;
