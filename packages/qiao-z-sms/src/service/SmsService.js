f5fb484c3fa8945b9a7e24e0ae5901399b6303f1
uikoo9 <uikoo9@qq.com>
2025-05-07 20:13:09 +0800

    feat(qiao-z-sms): 替换签名

--- packages/qiao-z-sms/src/service/SmsService.js
+++ packages/qiao-z-sms/src/service/SmsService.js
@@ -12,7 +12,7 @@ exports.sms = async (req, res) => {
   // const
   const mobile = req.body.mobile;
   const code = randomNumber(6);
-  const content = `【小楼AI】验证码：${code}，如非本人操作，请忽略此短信。`;
+  const content = `【北京洵美且好科技有限公司】验证码：${code}，如非本人操作，请忽略此短信。`;
 
   // send
   const json = await sendSms(
