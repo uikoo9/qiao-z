08ac660a55f038c61e716a5a11f9446adf03926d
uikoo9 <uikoo9@qq.com>
2025-07-16 15:45:12 +0800

    feat(qiao-z-): rate limit add userid

--- packages/qiao-z/src/init/rate-limit.js
+++ packages/qiao-z/src/init/rate-limit.js
@@ -11,8 +11,12 @@ export const rateLimitCheck = (req, res, rateLimit, rateLimitMaxCount) => {
   const ip = req.ip;
   if (!ip) return true;
 
+  // rateLimitKey
+  const userid = req.headers?.userid;
+  const rateLimitKey = userid ? `${ip}-${userid}` : ip;
+
   // rate limit
-  const rateLimitRes = rateLimit(ip, rateLimitMaxCount);
+  const rateLimitRes = rateLimit(rateLimitKey, rateLimitMaxCount);
   if (rateLimitRes) {
     res.jsonFail('rate limit');
     return;
