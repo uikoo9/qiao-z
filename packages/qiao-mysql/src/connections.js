c300dc23e4465217805dbfb0dbf20b422685aae5
uikoo9 <uikoo9@qq.com>
2025-09-11 20:09:59 +0800

    feat(qiao-mysql): fix connections log

--- packages/qiao-mysql/src/connections.js
+++ packages/qiao-mysql/src/connections.js
@@ -9,18 +9,23 @@ export const watchConnections = (app) => {
   // check
   if (!app.config.connectionLimit || !app.config.watch) return;
 
+  // const
+  const intervalTime = app.config.watchInterval || 1000;
+
   // pool
   const pool = app.pool;
   setInterval(() => {
-    const poolState = pool._allConnections.length;
-    const activeConnections = pool._activeConnections.length;
-    const idleConnections = pool._idleConnections.length;
-    const waitingConnections = pool._connectionQueue.length;
+    const totalConnections = pool._allConnections.length;
+    const idleConnections = pool._freeConnections?.length || 0;
+    const activeConnections = totalConnections - idleConnections;
+    const waitingConnections = pool._connectionQueue?.length || 0;
 
     // logs
-    console.log(methodName, '总连接数', poolState);
-    console.log(methodName, '活跃连接数', activeConnections);
-    console.log(methodName, '空闲连接数', idleConnections);
-    console.log(methodName, '等待队列长度', waitingConnections);
-  }, 1000);
+    console.log(methodName, 'time', new Date().getTime());
+    console.log(methodName, 'totalConnections', totalConnections);
+    console.log(methodName, 'idleConnections', idleConnections);
+    console.log(methodName, 'activeConnections', activeConnections);
+    console.log(methodName, 'waitingConnections', waitingConnections);
+    console.log();
+  }, intervalTime);
 };
