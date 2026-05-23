5436f3dd2e6e92b6b9862bd2fca380a6524997a1
uikoo9 <uikoo9@qq.com>
2026-05-23 20:19:13 +0800

    fix(qiao-z-nuser): 其实不是问题,瞎折腾

--- packages/qiao-z-nuser/src/service/UserInfoService.js
+++ packages/qiao-z-nuser/src/service/UserInfoService.js
@@ -8,15 +8,10 @@ const { userInfo } = require('qiao-z-service');
  */
 exports.userInfoFn = async (req, res) => {
   // send
-  const json = await userInfo(
-    {
-      url: global.QZ_CONFIG.user.url,
-      userid: req.headers.userid,
-    },
-    {
-      userid: req.headers.userid,
-    },
-  );
+  const json = await userInfo({
+    url: global.QZ_CONFIG.user.url,
+    userid: req.headers.userid,
+  });
 
   // send
   res.json(json);
